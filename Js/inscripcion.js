1869cebd5c519c254b382aba0eda43c8b17b2111
Ophiuco <mica.neiff@gmail.com>
2019-11-25 20:32:32 -0300

    modicicaciones en inscripcion

--- Js/inscripcion.js
+++ Js/inscripcion.js
@@ -1,43 +1,52 @@
 //clonar y eliminar el formulario
 $(document).ready(function(){
 
-    $(".formulario .clonaar").click(function(){
+    $(".main_planilla_form .clonar").click(function(){
     	var clonar=$(this).parent(".a_clonar").clone(true);
         $(this).parent(".a_clonar").after(clonar);
     });
         
-    $(".formulario .eliminar").click(function(){
+    $(".main_planilla_form .eliminar").click(function(){
     	if($(".a_clonar").length>1){
     	$(this).parent(".a_clonar").remove();
     	}
     });
+
 });
 
-//seleccion multiple
+
+//agregar al total
+var cant=1;
 $(document).ready(function(){
-	$(".seleccion-curso").select2({
-  	tags: true
+	$("#sumar").click(function(){
+		var precio=4830;
+		cant++;
+		$("#total").text(precio*cant);
+	});
+	
+});
+
+$(document).ready(function(){
+		$("#restar").click(function(){
+		var precio=4830;
+		$("#total").text(("#total").val()-precio);
 	});
 });
 
+//MODAL CON RESUMEN
+
+function mostrarVentana(){
+	document.getElementById("miVentana").style.display = "block";
+
+}
+function ocultarVentana(){
+    document.getElementById("miVentana").style.display = "none";
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//para probar si andan los botones clonar y eliminar
-$(document).ready(function(){
-
-	$(".mensaje").click(function(){
-		alert("el boton anda");
-		});
-});
+/*function capturar(){
+	var nombre=document.getElementById("nombre_1").value;
+	var apellido=document.getElementById("apellido_1").value;
+	var dni=document.getElementById("dni_1").value;
+	document.getElementById("resultado").innerHTML=nombre+" "+apellido+" "+dni;     
+}*/
