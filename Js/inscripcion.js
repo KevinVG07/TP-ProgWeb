11b42fec5864a9537463a2827c756652b43f74dd
Ophiuco <mica.neiff@gmail.com>
2019-11-25 23:53:13 -0300

    clonacion, suma y resta arreglados

--- Js/inscripcion.js
+++ Js/inscripcion.js
@@ -10,27 +10,33 @@ $(document).ready(function(){
     	if($(".a_clonar").length>1){
     	$(this).parent(".a_clonar").remove();
     	}
+    	else{$(".a_clonar")[0].reset() }
     });
 
 });
 
 
 //agregar al total
-var cant=1;
 $(document).ready(function(){
 	$("#sumar").click(function(){
+		var precioActual=parseInt($("#total").text());
 		var precio=4830;
-		cant++;
-		$("#total").text(precio*cant);
+		$("#total").text(precioActual+precio);
 	});
 	
 });
 
 $(document).ready(function(){
 		$("#restar").click(function(){
+		var precioActual=parseInt($("#total").text());
 		var precio=4830;
-		$("#total").text(("#total").val()-precio);
-	});
+		if (precioActual==4830){
+				$("#total").text(precioActual);
+				}
+				else{
+				$("#total").text(precioActual-precio);	
+				} 	
+		});
 });
 
 //MODAL CON RESUMEN
@@ -44,9 +50,9 @@ function ocultarVentana(){
 }
 
 
-/*function capturar(){
+function capturar(){
 	var nombre=document.getElementById("nombre_1").value;
 	var apellido=document.getElementById("apellido_1").value;
 	var dni=document.getElementById("dni_1").value;
 	document.getElementById("resultado").innerHTML=nombre+" "+apellido+" "+dni;     
-}*/
+}
